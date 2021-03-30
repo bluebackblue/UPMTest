91602a0b280af5329f7bc8f8dda210721fa1c38c
blueback <blueback28@gmail.com>
2021-03-30 13:30:47 +0900

    初期コミット

---
+++ unity_upmtest/Assets/Samples/AssetLib/0.0.1/Sample/Main_MonoBehaviour.cs
@@ -0,0 +1,24 @@
+
+
+/** Samples.AssetTool.Sample
+*/
+namespace Samples.AssetTool.Sample
+{
+	/** Main_MonoBehaviour
+	*/
+	public class Main_MonoBehaviour : UnityEngine.MonoBehaviour
+	{
+		/** Start
+		*/
+		private void Start()
+		{
+		}
+
+		/** Update
+		*/
+		private void Update()
+		{
+		}
+	}
+}
+
