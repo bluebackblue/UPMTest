ed13c389affe3950f4708634dd7784e89695a486
blueback <blueback28@gmail.com>
2021-04-04 14:28:27 +0900

    テスト

---
+++ unity_upmtest/Assets/Samples/SlackWebApi/0.0.2/IncomingWebhooks/Editor/MenuItem.cs
@@ -0,0 +1,91 @@
+
+
+/** Samples.SlackWebApi.IncomingWebhooks.Editor
+*/
+namespace Samples.SlackWebApi.IncomingWebhooks.Editor
+{
+	/** MenuItem
+	*/
+	#if(UNITY_EDITOR)
+	public class MenuItem
+	{
+		/** CoroutineDummy
+		*/
+		public class CoroutineDummy
+		{
+			/** sendtext
+			*/
+			private BlueBack.SlackWebApi.IncomingWebhooks.SendText sendtext;
+
+			/** constructor
+			*/
+			public CoroutineDummy()
+			{
+				UnityEngine.Debug.Log("Start");
+
+				//サンプル用。
+				//「https://api.slack.com/apps」でWebhookURLを取得して下記のＵＲＬを差し替える。
+				string t_webhookurl = "https://hooks.slack.com/services/T00000000/B0000000000/000000000000000000000000";
+
+				//開発用。
+				#if(DEF_USER_BLUEBACK)
+				{
+					BlueBack.JsonItem.JsonItem t_jsonitem = new BlueBack.JsonItem.JsonItem(BlueBack.AssetLib.LoadText.LoadTextFromAssetsPath("../../../config/SlackWebApi.txt"));
+					t_webhookurl = t_jsonitem.GetItem("webhookurl").GetStringData();
+				}
+				#endif
+
+				this.sendtext = new BlueBack.SlackWebApi.IncomingWebhooks.SendText(t_webhookurl	,"あいうえお");
+			}
+
+			/** Update
+			*/
+			public void Update()
+			{
+				if(this.sendtext != null){
+					this.sendtext.Update();
+					switch(this.sendtext.mode){
+					case BlueBack.SlackWebApi.IncomingWebhooks.SendText.Mode.Request:
+					case BlueBack.SlackWebApi.IncomingWebhooks.SendText.Mode.Work:
+						{
+						}return;
+					}
+				}
+
+				//終了。
+				UnityEditor.EditorApplication.update -= Update;
+				
+				{
+					UnityEngine.Debug.Log("mode : " + this.sendtext.mode.ToString());
+
+					if(this.sendtext.result != null){
+						UnityEngine.Debug.Log("result : " + this.sendtext.result);
+					}
+
+					if(this.sendtext.errorstring != null){
+						UnityEngine.Debug.Log("errorstring : " + this.sendtext.errorstring);
+					}
+
+					UnityEngine.Debug.Log(this.sendtext.webrequest.responseCode.ToString());
+					foreach(var t_pair in this.sendtext.webrequest.GetResponseHeaders()){
+						UnityEngine.Debug.Log(t_pair.Key + " : " + t_pair.Value);
+					}
+				}
+
+				UnityEngine.Debug.Log("End");
+				this.sendtext.DisposeWebRequest();
+				this.sendtext = null;
+			}
+		}
+
+		/** テスト。
+		*/
+		[UnityEditor.MenuItem("サンプル/SlackWebApi/IncomingWebhooks/Test")]
+		private static void MenuItem_Sample_SlackWebApi_IncomingWebhooks_Test()
+		{
+			UnityEditor.EditorApplication.update += new CoroutineDummy().Update;
+		}
+	}
+	#endif
+}
+
